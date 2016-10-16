ff0d901a8467e678799c2e9363c43f2d54242297
Marcus Eisele <marcus.eisele@gmail.com>
2016-10-16 08:46:21 +0000

    removed alert from gui

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $("#pause").click(function(){
-        alert("click");
         $.post( "api/buttons/pause", function( data ) {
             console.log("succesful: pause");
         });
